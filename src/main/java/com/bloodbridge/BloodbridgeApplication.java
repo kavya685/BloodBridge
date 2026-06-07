02858ee400ddafec5c24ffb8390efe959b0fb212
kavya685 <kavyapopat95@gmail.com>
2026-06-07 12:47:43 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/bloodbridge/BloodbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.bloodbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodbridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloodbridge/BloodbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
