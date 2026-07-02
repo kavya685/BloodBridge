edd5f0b31dea1c8e2e8913eae669803e4590adc5
kavya685 <kavyapopat95@gmail.com>
2026-07-02 17:12:50 +0530

    feat: update filter logic with some additions

--- src/main/java/com/bloodbridge/security/JwtFilter.java
+++ src/main/java/com/bloodbridge/security/JwtFilter.java
@@ -12,13 +12,29 @@ import java.io.IOException;
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService)
+    {
+        this.jwtService = jwtService;
+    }
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain)
             throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
 
+        // checking bearer is required coz jwt token is sent in the header as "Bearer <token>"
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = authHeader.substring(7);
+        String email = jwtService.extractUsername(jwt);
     }
 
 
